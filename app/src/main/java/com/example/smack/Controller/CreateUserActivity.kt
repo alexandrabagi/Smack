7bbe976328c5eed7eed6e9fa4fa63cdb0a27be29
AlexandraBagi <szandra.bagi@gmail.com>
2019-12-30 15:37:10 +0100

    First web request in CreateUserActivity

--- app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
+++ app/src/main/java/com/example/smack/Controller/CreateUserActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smack.R
+import com.example.smack.Services.AuthService
 import kotlinx.android.synthetic.main.activity_create_user.*
 import java.util.*
 
@@ -48,6 +49,10 @@ class CreateUserActivity : AppCompatActivity() {
     }
 
     fun createUserClicked(view: View) {
+        AuthService.registerUser(this, "j@j.com", "123456") {complete ->
+            if (complete) {
 
+            }
+        }
     }
 }
